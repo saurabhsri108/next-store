6f443429b0a8a32b9955cc36ffdb1d8500665fb2
Saurabh Srivastava <vasudeveloper001@gmail.com>
2023-06-05 14:02:45 +0530

    Customised the Account Dashboard page

--- src/containers/DashboardLayout.tsx
+++ src/containers/DashboardLayout.tsx
@@ -1,20 +1,22 @@
 import {ReactNode} from "react";
 import Container from "@/components/Container";
 import Link from "next/link";
+import {useRouter} from "next/router";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <Container className="p-4 flex justify-center">
-    <aside className="w-[25%]">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/dashboard/profile">Dashboard</Link>
+  const {asPath} = useRouter()
+  return <Container className="p-4 flex justify-center flex-col md:flex-row">
+    <aside className="w-full md:w-[20%] md:mr-2 border-r-2 border-r-slate-100">
+      <nav className="w-full md:w-[94%]">
+        <ul className="flex flex-row md:flex-col gap-2 w-full">
+          <li className="w-full text-center md:text-left">
+            <Link className={`${asPath === "/dashboard/profile" ? "bg-slate-950 text-slate-50" : "bg-slate-200 text-slate-950"}  inline-block w-full p-2 border-b-2 border-b-white`} href="/dashboard/profile">Profile</Link>
           </li>
-          <li>
-            <Link href="/dashboard/orders">Orders</Link>
+          <li className="w-full text-center md:text-left">
+            <Link className={`${asPath === "/dashboard/orders" ? "bg-slate-950 text-slate-50" : "bg-slate-200 text-slate-950"}  inline-block w-full p-2 border-b-2 border-b-white`} href="/dashboard/orders">Orders</Link>
           </li>
-          <li>
-            <Link href="/dashboard/refunds" onClick={() => {
+          <li className="w-full text-center md:text-left">
+            <Link className={`${asPath === "/dashboard/refunds" ? "bg-slate-950 text-slate-50" : "bg-slate-200 text-slate-950"}  inline-block w-full p-2 border-b-2 border-b-white`} href="/dashboard/refunds" onClick={() => {
               // @ts-ignore
               smartech("identify", "saurabhsri+123@geekyants.com");
               // @ts-ignore
@@ -27,7 +29,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         </ul>
       </nav>
     </aside>
-    <section className="w-[75%]">
+    <section className="w-full md:w-[80%]">
       {children}
     </section>
   </Container>;
